3e9281189c384456eedadb91d577cb83fe1abc16
Techmokid <58498119+Techmokid@users.noreply.github.com>
2023-10-25 00:55:14 +0800

    Added .dll files

--- NEW Cpp Setup - Starter Kit/CompileAndRun.py
+++ NEW Cpp Setup - Starter Kit/CompileAndRun.py
@@ -1,7 +1,38 @@
 import subprocess,os
 
 print("[COMPILER] Starting compilation...")
-result = subprocess.run("g++ *.cpp *.h -o RunProg.exe", stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.decode('utf-8')
+
+def GetLib(x):
+    return "-l"+x
+
+commandArgs = []
+commandArgs.append(r'g++ *.cpp *.h')
+#commandArgs.append(r'-I"F:\OpenGL Libraries\freeglut\include"')
+#commandArgs.append(r'-L"F:\OpenGL Libraries\freeglut\lib\x64"')
+#commandArgs.append(r'-I"F:\OpenGL Libraries\glew-2.1.0\include"')
+#commandArgs.append(r'-L"F:\OpenGL Libraries\glew-2.1.0\lib\Release\x64"')
+#commandArgs.append(r'-I"F:\OpenGL Libraries\glfw-3.3.8.bin.WIN64\include"')
+#commandArgs.append(r'-L"F:\OpenGL Libraries\glfw-3.3.8.bin.WIN64\lib-mingw-w64"')
+#commandArgs.append(GetLib('opengl32'))
+#commandArgs.append(GetLib('pthread'))
+#commandArgs.append(GetLib('freeglut'))
+#commandArgs.append(GetLib('glfw3'))
+#commandArgs.append(GetLib('glu32'))
+#commandArgs.append(GetLib('freeGLUT'))
+#commandArgs.append(GetLib('glew32'))
+#commandArgs.append(GetLib('ws2_32'))
+#commandArgs.append(GetLib('gdi32'))
+commandArgs.append('-static-libstdc++')
+commandArgs.append('-std=c++17')
+commandArgs.append('-fconcepts')
+commandArgs.append('-g')
+commandArgs.append('-o RunProg.exe')
+
+command = ""
+for i in commandArgs:
+    command += i + " "
+
+result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT).stdout.decode('utf-8')
 
 if (result != ""):
     print("[COMPILER] A compilation error has occured")
